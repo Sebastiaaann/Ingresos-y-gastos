a143fdfe80d1d5c5439d94d5629f07a6be9e43e1
Sebastiaaann <sebastian.almonacid06@inacapmail.cl>
2023-11-25 18:12:01 -0300

    3 commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-expense-tracker/',
+  base: '/Ingresos-y-gastos/',
   plugins: [react()],
 })
 
