3fa4169ac2dfe2506b0d3cd87d2eded1019abe66
Sebastiaaann <sebastian.almonacid06@inacapmail.cl>
2023-12-19 00:18:06 -0300

    Descargar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,55 @@
-import { GlobalProvider } from "./context/GlobalState";
+import { GlobalProvider, useGlobalState } from "./context/GlobalState";
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import { IncomeExpenses } from "./components/IncomeExpenses";
 import { TransactionList } from "./components/transactions/TransactionList";
 import { TransactionForm } from "./components/transactions/TransactionForm";
 import { ExpenseChart } from "./components/ExpenseChart";
+import { saveAs } from 'file-saver';
+import { utils, writeFile } from 'xlsx';
 
-// Define el componente principal 'App'
 function App() {
+  const { transactions, balance } = useGlobalState();
+
+  function downloadTransactions() {
+    // Calcula los ingresos y gastos
+    const income = transactions
+      .filter(transaction => transaction.amount > 0)
+      .reduce((acc, transaction) => acc + transaction.amount, 0);
+    const expense = transactions
+      .filter(transaction => transaction.amount < 0)
+      .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+    // Crea un array con los datos que quieres guardar
+    const dataToSave = transactions.map(transaction => ({
+      description: transaction.text,
+      amount: transaction.amount,
+      income: transaction.amount > 0 ? transaction.amount : 0,
+      expense: transaction.amount < 0 ? transaction.amount : 0,
+    }));
+
+    // Añade los totales al final del array
+    dataToSave.push({
+      description: 'Total',
+      amount: balance,
+      income,
+      expense,
+    });
+
+    // Convierte los datos a una hoja de cálculo
+    const worksheet = utils.json_to_sheet(dataToSave, { header: ['description', 'amount', 'income', 'expense'] });
+
+    // Crea un nuevo libro y añade la hoja de cálculo a él
+    const workbook = utils.book_new();
+    utils.book_append_sheet(workbook, worksheet, 'Transactions');
+
+    // Escribe el libro a un archivo y lo guarda
+    const excelBuffer = writeFile(workbook, 'transactions.xlsx', { bookType: 'xlsx', type: 'array' });
+    const data = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8' });
+    saveAs(data, 'transactions.xlsx');
+  }
+
   return (
-    // Usa el contexto GlobalProvider para envolver la aplicación
     <GlobalProvider>
       <div className="bg-neutral-950 text-white h-screen flex justify-center items-center">
         <div className="w-2/5 flex justify-center items-center">
@@ -20,12 +60,14 @@ function App() {
                 <IncomeExpenses />
                 <Balance />
                 <TransactionForm />
-                {/* Agrega el botón con el enlace externo */}
                 <a href="http://127.0.0.1:8000/" target="_blank" rel="noopener noreferrer">
                   <button className="bg-indigo-700 text-white px-3 py-2 rounded-lg block mb-2 w-full">
                     Volver a inicio
                   </button>
                 </a>
+                <button onClick={downloadTransactions} className="bg-indigo-700 text-white px-3 py-2 rounded-lg block mb-2 w-full">
+                  Descargar montos de transacciones
+                </button>
               </div>
               <div className="flex-1 flex flex-col">
                 <ExpenseChart />
@@ -38,5 +80,5 @@ function App() {
     </GlobalProvider>
   );
 }
-// Exporta el componente 'App' para su uso en otros componentes
+
 export default App;
